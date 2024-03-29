b1af9d2aa6cdb7ac34abc46feb71dc599371e158
AF-CYBER-409 <147817752+AF-CYBER-409@users.noreply.github.com>
2024-03-30 04:11:44 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,6 +1,6 @@
 import os
-import Ganza
-Ganza.login()
+import R
+R.login()
 #---------------পারলে ডিকোড করে দেখা--------------#
 #------------------Abal-not-----------------#
 #--------------Fuck-your-decoder-mom---------------#
