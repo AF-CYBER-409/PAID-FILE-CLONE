7047407992585afd6d6200be7f0acc52e673e21a
AF-CYBER-409 <147817752+AF-CYBER-409@users.noreply.github.com>
2024-04-05 21:53:29 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,6 +1,6 @@
 import os
-import tu
-tu.login()
+import spkk
+spkk.login()
 #---------------পারলে ডিকোড করে দেখা--------------#
 #------------------Abal-not-----------------#
 #--------------Fuck-your-decoder-mom---------------#
