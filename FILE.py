edf2b15d3bf3c7ecc2a0c56c14826545d1098920
AF-CYBER-409 <147817752+AF-CYBER-409@users.noreply.github.com>
2024-04-10 22:40:08 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,6 +1,6 @@
 import os
-import eee
-eee.login()
+import python 
+python.login()
 #---------------পারলে ডিকোড করে দেখা--------------#
 #------------------Abal-not-----------------#
 #--------------Fuck-your-decoder-mom---------------#
